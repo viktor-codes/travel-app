13bb9cc50d9394412724f5b8df3be0c6abaa3f8d
Viktor Rura <ruravictor@gmail.com>
2025-10-21 19:34:23 +0100

    Add some links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,28 @@
+import { NAV_LINKS } from '@/constants'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Navbar = () => {
 	return (
-		<nav className='border-2 border-red-500 flex justify-between z-30 py-5'>
-			<Link href='/' className='text-2xl font-bold ml-5'>
-				Travel
+		<nav className='flex justify-between z-30 py-5 container mx-auto'>
+			<Link href='/'>
+				<Image
+					src='/hilink-logo.svg'
+					alt='HiLink Logo'
+					width={74}
+					height={29}
+				/>
+				<ul className='hidden h-full gap-12 lg:flex'>
+					{NAV_LINKS.map(link => (
+						<Link
+							href={link.href}
+							key={link.key}
+							className='flex items-center font-normal text-lg cursor-pointer pb-1.5 transition-all hover:font-bold'
+						>
+							{link.label}
+						</Link>
+					))}
+				</ul>
 			</Link>
 		</nav>
 	)
